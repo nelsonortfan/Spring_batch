3a9aa807352c7b72452c80595be6c50ce0a6c9c4
Nelson <n.ortizf@uniandes.edu.co>
2025-01-23 16:14:15 -0500

    Cambiando BD H2 por MySQL y ejecucion exitosa

--- springbatch/src/main/java/com/example/springbatch/SpringbatchApplication.java
+++ springbatch/src/main/java/com/example/springbatch/SpringbatchApplication.java
@@ -3,7 +3,7 @@ package com.example.springbatch;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
 public class SpringbatchApplication {
--- springbatch/src/main/java/com/example/springbatch/config/SimpleTaskConfig.java
+++ springbatch/src/main/java/com/example/springbatch/config/SimpleTaskConfig.java
@@ -2,15 +2,21 @@ package com.example.springbatch.config;
 
 import com.example.springbatch.service.SecondTasklet;
 import com.example.springbatch.task.SimpleTask;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.batch.BatchDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SimpleTaskConfig {
@@ -23,6 +29,15 @@ public class SimpleTaskConfig {
     private final String SIMPLE_TASK = "Simple task of mine";
 
     private final String SECOND_TASK = "Second simple task of mine";
+
+    @Value("${batchDatasource.driver}")
+    private String batchDatasourceDriver;
+    @Value("${batchDatasource.url}")
+    private String batchDatasourceUrl;
+    @Value("${batchDatasource.username}")
+    private String batchDatasourceUsername;
+    @Value("${batchDatasource.password}")
+    private String batchDatasourcePassword;
     @Bean
     public Job simpleTaskJob(JobRepository jobRepository, Step simpleTaskStep, Step secondTaskStep){
         return new JobBuilder(SIMPLE_JOB,jobRepository)
@@ -44,6 +59,19 @@ public class SimpleTaskConfig {
                 .tasklet(secondTasklet, platformTransactionManager)
                 .allowStartIfComplete(true)
                 .build();
+    }
+
+    @Bean
+    @BatchDataSource
+    // @BatchDataSource specifies that this bean will be used as the
+    // data source for the ItemReader during batch processing
+    public DataSource batchDatasource() {
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName(batchDatasourceDriver);
+        config.setJdbcUrl(batchDatasourceUrl);
+        config.setUsername(batchDatasourceUsername);
+        config.setPassword(batchDatasourcePassword);
+        return new HikariDataSource(config);
     }
 
 }
